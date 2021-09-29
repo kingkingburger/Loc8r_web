a03ad364333cf59c2cd55189175e233dc4dcd8b4
kingkingburger <dnjsalsgh123@gmail.com>
2021-09-29 18:47:27 +0900

    "6-2"

---
+++ app_server/models/db.js
@@ -0,0 +1,42 @@
+require('./locations')
+const mongoose = require('mongoose');
+var gracefulShutdown;
+const dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI, {userNewUrlParser: true});
+
+mongoose.connection.on('connected',function(){
+    console.log('Mongoose connented to' + dbURI);
+});
+
+mongoose.connection.on('error',function(err){
+    console.log('Mongoose connented err' + dbURI);
+});
+
+mongoose.connection.on('disconnected',function(){
+    console.log('Mongoose disconnented to' + dbURI);
+});
+
+var gracefulShutdown = function (msg, callback){
+    mongoose.connection.close(function(){
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+}
+
+process.once('SIGUSR2', function(){
+    gracefulShutdown('nodemon restart',function(){
+        process.kill(process.pid,'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', function(){
+    gracefulShutdown('app termination shut donw',function(){
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function(){
+    gracefulShutdown('Heroku app shut donw',function(){
+        process.exit(0);
+    });
+});
