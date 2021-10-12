110a86618ce5a1d75a28a9c4b9a95b3846355d6d
kingkingburger <dnjsalsgh123@gmail.com>
2021-10-12 15:20:06 +0900

    10-5

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -2,11 +2,77 @@ const mongoose = require("mongoose");
 const Loc = mongoose.model('Location');
 
 const locationsReadOne = (req, res) => {
-    res
-        .status(200)
-        .json({"status":"success"});
+    Loc
+        .findById(req.params.locationid)
+        .exec((err, location) => {
+            if(!location){
+                return res
+                    .status(404)
+                    .json({
+                        "message":"lcoation not found"
+                    });
+            } else if (err){
+                return res
+                    .status(404)
+                    .json(err);
+            }
+            res
+                .status(200)
+                .json(location);
+        
+        });
 };
-const locationsListByDistance = (req, res) =>{};
+
+
+const locationsListByDistance = async (req, res) =>{
+    const lng = parseFloat(req.query.lng);
+    const lat = parseFloat(req.query.lat);
+    const near = {
+        type: "Point",
+        coordinates : [lng, lat]
+    };
+    const geoOptions = {
+        distanceField: "distance.calculated",
+        key: 'coords',
+        spherical: true,
+        maxDistance: 200000
+    };
+    
+    if(!lng || !lat){
+        return res
+            .status(404)
+            .json({"message": "lng and lat query parameter are required"});
+    }
+    try{
+        const results = await Loc.aggregate([
+            {
+                $geoNear: {
+                    near,
+                    ...geoOptions
+                }
+            }
+        ]);
+        const locations = results.map(result => {
+            return{
+                _id: result._id,
+                name: result.name,
+                address: result.address,
+                rating: result.rating,
+                facilities: result.facilities,
+                distance: `${result.distance.calculated.toFixed()}m`
+            }
+        });
+        res
+            .status(200)
+            .json(locations);
+    } catch(err){
+        console.log(err);
+        res
+            .status(404)
+            .json(err);
+    }
+};
+
 const locationsCreate = (req, res) =>{};
 const locationsUpdateOne = (req, res) =>{};
 const locationsDeleteOne = (req, res) =>{};
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -2,7 +2,50 @@ const mongoose = require("mongoose");
 const Loc = mongoose.model('Location');
 
 const reviewCreate = (req, res) => {};
-const reviewsReadOne = (req, res) =>{};
+const reviewsReadOne = (req, res) =>{
+    Loc
+        .findById(req.parmas.locationid)
+        .select('name reviews')
+        
+        .exec((err, location) =>{
+            if(!location){
+                return res
+                    .status(404)
+                    .json({
+                        "message":"lcoation not found"
+                    });
+            } else if (err){
+                return res
+                    .status(404)
+                    .json(err);
+            }
+            if(location.reviews && location.reviews.length >0){
+                const review = location.reviews.id(req.parmas.reviewid);
+
+                if(!review){
+                    return res
+                        .status(404)
+                        .json({"message": "review not found"});
+                }else{
+                    const response={
+                        location: {
+                            name: location.name,
+                            id: req.parmas.locationid
+                        },
+                        review
+                    };
+
+                    return res
+                        .status(200)
+                        .json(response)
+                }
+            }else{
+                return res
+                    .status(404)
+                    .json({"message": "No reviews found"});
+            }
+        });
+};
 const reviewsUpdateOne = (req, res) =>{};
 const reviewsDeleteOne = (req, res) =>{};
 
