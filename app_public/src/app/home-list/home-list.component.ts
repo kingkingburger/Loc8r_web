0da24f08f9aa184e31623b3b84453fb07a1f8602
kingkingburger <dnjsalsgh123@gmail.com>
2021-11-09 13:44:57 +0900

    chap 16

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Loc8rDataService } from '../loc8r-data.service';
 
 export class Location {
   _id?: string;
-  name?: string;
+  name?: string = '';
   distance: number = 0;
   address?: string;
   rating: number = 0;
@@ -18,27 +19,17 @@ export class Location {
 
 export class HomeListComponent implements OnInit {
 
-  constructor() { }
-  name = 'Burger Queen';
+  constructor(private loc8rDataService: Loc8rDataService) { }
+  public locations: Location[] = [];
 
-  locations: Location[]=[{
-    _id: '5f2a3b39e7304b02484222d3',
-    name: 'Burger Queen',
-    distance: 5100.0,
-    address: '서울특별시 관악구 봉천동 남부순환로 1924',
-    rating: 3,
-    facilities: ['Hot drinks','Premium wifi']
-  }, {
-    _id: '5f2a3b39e7304b02484222d3',
-    name: 'Starcups',
-    distance: 120.542,
-    address: 'High Street, Reaing',
-    rating: 5,
-    facilities: ['Hot drinks','Premium wifi', 'food']
+  private getLocations(): void{
+    this.loc8rDataService
+      .getLocations()
+        .then(foundLocations => this.locations = foundLocations);
   }
-];
 
   ngOnInit(): void {
+    this.getLocations();
   }
 
 }
---
+++ app_public/src/app/loc8r-data.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Location } from './home-list/home-list.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class Loc8rDataService {
+  constructor(private http: HttpClient) { }
+
+  private apiBaseUrl = 'http://localhost:3000/api';
+
+  public getLocations(): Promise<Location[]> {
+    const lng: number = 126.941387;
+    const lat: number = 37.473339;
+    const maxDistance: number = 20000;
+    const url: string = `${this.apiBaseUrl}/locations?lng=${lng}&lat=${lat}&maxDistance=${maxDistance}`;
+    return this.http
+      .get(url)
+      .toPromise()
+      .then(response => response as Location[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any>{
+    console.error('Something has gone wrong', error);
+    return Promise.reject(error.message || error);
+  }
+}
